a60e93632f3a771ed223f04cc7c80e10f61227b4
Fanny HUJMAN <hujman.fanny@gmail.com>
2025-08-19 18:03:07 +0200

    add 2mb

--- orkestre-app/src/environments/environment.development.ts
+++ orkestre-app/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
  export const environment = {
      production: false,
-   //apiURL: 'https://orkestre-api-212f44656fcc.herokuapp.com'
-   apiURL: 'http://localhost:8000'
+   apiURL: 'https://orkestre-api-212f44656fcc.herokuapp.com'
+  // apiURL: 'http://localhost:8000'
  };
